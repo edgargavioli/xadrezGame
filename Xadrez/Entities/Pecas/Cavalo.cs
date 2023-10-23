29939572c8409cdf35b202244b8e519c539ba174
edgar <edgargavioli@outlook.com>
2023-10-23 13:39:53 -0300

    Adicionando as demais pecas

---
+++ Xadrez/Entities/Pecas/Cavalo.cs
@@ -0,0 +1,74 @@
+﻿using Xadrez.Entities.Tabuleiros;
+
+namespace Xadrez.Entities.Pecas
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca peca = Tab.peca(pos);
+            return peca == null || peca.CorP != CorP;
+        }
+
+        public override bool[,] MovesP()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //Acima
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha - 2, posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha - 2, posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha + 2, posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha + 2, posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+
+            return mat;
+        }
+    }
+}
---
+++ Xadrez/Entities/Pecas/Peao.cs
@@ -0,0 +1,78 @@
+﻿using Xadrez.Entities.Tabuleiros;
+
+namespace Xadrez.Entities.Pecas
+{
+    internal class Peao : Peca
+    {
+        public Peao(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "P";
+        }
+        private bool existeInimigo(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p != null && p.CorP != CorP;
+        }
+        private bool livre(Posicao pos)
+        {
+            return Tab.peca(pos) == null;
+        }
+
+        public override bool[,] MovesP()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+            if (CorP == Cor.Branca)
+            {
+                pos.definirValores(pos.Linha - 1, pos.Coluna);
+                if(Tab.PosicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha - 2, pos.Coluna);
+                if (Tab.PosicaoValida(pos) && livre(pos) && QuantidadeMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna - 1);
+                if (Tab.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha - 1, pos.Coluna + 1);
+                if (Tab.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            else
+            {
+                pos.definirValores(pos.Linha + 1, pos.Coluna);
+                if (Tab.PosicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha + 2, pos.Coluna);
+                if (Tab.PosicaoValida(pos) && livre(pos) && QuantidadeMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna - 1);
+                if (Tab.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(pos.Linha + 1, pos.Coluna + 1);
+                if (Tab.PosicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            return mat;
+        } 
+    }
+}
